42f8b2f41bd93cab32774c1ce4528b4c720bb1da
gudvin <swmmci@gmail.com>
2025-05-09 22:50:34 +0200

    mcphub repaired

--- nvim/lua/plugins/mcphub.lua
+++ nvim/lua/plugins/mcphub.lua
@@ -1,29 +1,14 @@
 return {
-  "ravitemer/mcphub.nvim",
-  dependencies = {
-    "nvim-lua/plenary.nvim", -- Required for Job and HTTP requests
-  },
-  -- cmd = "MCPHub", -- lazily start the hub when `MCPHub` is called
-  build = "npm install -g mcp-hub@latest", -- Installs required mcp-hub npm module
-  config = function()
-    require("mcphub").setup({
-      -- Required options
-      port = 3000, -- Port for MCP Hub server
-      config = vim.fn.expand("~/mcpservers.json"), -- Absolute path to config file
-
-      -- Optional options
-      on_ready = function(hub)
-        -- Called when hub is ready
-      end,
-      on_error = function(err)
-        -- Called on errors
-      end,
-      log = {
-        level = vim.log.levels.WARN,
-        to_file = false,
-        file_path = nil,
-        prefix = "MCPHub",
-      },
-    })
-  end,
+    "ravitemer/mcphub.nvim",
+    dependencies = {
+      "nvim-lua/plenary.nvim",  -- Required for Job and HTTP requests
+    },
+    -- uncomment the following line to load hub lazily
+    --cmd = "MCPHub",  -- lazy load 
+    build = "npm install -g mcp-hub@latest",  -- Installs required mcp-hub npm module
+    -- uncomment this if you don't want mcp-hub to be available globally or can't use -g
+    -- build = "bundled_build.lua",  -- Use this and set use_bundled_binary = true in opts  (see Advanced configuration)
+    config = function()
+      require("mcphub").setup()
+    end,
 }
