dc0aa3e80058796551e32a2afe28f98972b11067
gudvin <swmmci@gmail.com>
2025-05-09 22:36:55 +0200

    added new config

---
+++ nvim/lua/plugins/lsp-minimal.lua
@@ -0,0 +1,27 @@
+return {
+  {
+    "williamboman/mason.nvim",
+    version = "*", -- Use latest version
+    config = true,
+  },
+  {
+    "williamboman/mason-lspconfig.nvim",
+    version = "*", -- Use latest version
+    dependencies = {
+      "williamboman/mason.nvim",
+    },
+    opts = {
+      automatic_installation = true,
+    },
+  },
+  {
+    "neovim/nvim-lspconfig",
+    dependencies = {
+      "williamboman/mason.nvim",
+      "williamboman/mason-lspconfig.nvim",
+    },
+    config = function()
+      require("lspconfig").marksman.setup({})
+    end,
+  },
+}
